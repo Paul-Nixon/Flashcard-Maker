9f48aabd05b1a290ffe1baeebf9eed5902be0572
Paul-Nixon <nixonp4@gmail.com>
2021-10-18 18:59:10 -0500

    still working on the About page

--- my-app/src/components/pages/About.js
+++ my-app/src/components/pages/About.js
@@ -8,12 +8,40 @@ export default function About() {
             <HomepageHeader />
 
             <div className={styles.aboutContent}>
-                <h2>About</h2>
+                <h2 className="page-title">About</h2>
 
                 <section>
                     <div className={styles.aboutMainContent}>
-                        <h2></h2>
-                        <p></p>
+                        <div className={styles.aboutDetails}>
+                            <h2 className={styles.aboutSectionTitle}>Make your own flashcards</h2>
+                            <p className={styles.aboutSectionText}>
+                                Need an easy and customizable way to study and retain information? When you login to
+                                your account, you can make an unlimited number of flashcards for free. 
+                            </p>
+                            <ul className={styles.aboutSectionList}>
+                                <li>Create categories which contain their own set of flashcards</li>
+                                <li>Edit &amp; delete existing flashcards</li>
+                            </ul>
+                        </div>
+                        
+                        <img />
+                    </div>
+
+                    <div className={styles.aboutMainContent}>
+                        <div className={styles.aboutDetails}>
+                            <h2 className={styles.aboutSectionTitle}>Quiz &amp; test yourself</h2>
+                            <p className={styles.aboutSectionText}>
+                                The quiz feature lets you conduct a quiz using a user-specified number of a category's
+                                flashcards. Also, the test feature conducts a test using all of a category's
+                                flashcards.
+                            </p>
+                            <ul className={styles.aboutSectionList}>
+                                <li>Can use an optional timer for quizzes and tests</li>
+                                <li>Score for every test and quiz can be viewed using the Scores tab</li>
+                            </ul>
+                        </div>
+
+                        <img />
                     </div>
                 </section>
             </div>
