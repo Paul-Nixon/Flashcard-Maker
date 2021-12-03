4d2721b547c77b958ca35b10056df619236fef1b
Paul-Nixon <nixonp4@gmail.com>
2021-12-02 19:02:01 -0600

    I hate CSS

--- my-app/src/components/flashcards/Flashcard.js
+++ my-app/src/components/flashcards/Flashcard.js
@@ -1,15 +1,32 @@
 import './Flashcard.css';
 
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 
 export default function Flashcard({ flashcard }) {
     
-    const [flip, setFlip] = useState(false);
-    
+    const [flip, setFlip] = useState(false),
+    [height, setHeight] = useState("initial"),
+    frontSide = useRef(),
+    backSide = useRef();
+
+    function setMaxHeight()
+    {
+        const frontHeight = frontSide.current.getBoundingClientRect().height
+        const backHeight = backSide.current.getBoundingClientRect().height
+        setHeight(Math.max(frontHeight, backHeight, 100))
+    }
+
+    useEffect(setMaxHeight, [flashcard.question, flashcard.answer, flashcard.options])
+    useEffect(() => {
+        window.addEventListener('resize', setMaxHeight)
+        return () => window.removeEventListener('resize', setMaxHeight)
+    }, [])
+
+
     return (
-        <div className={`flashcard ${flip ? "flip" : ""}`} onClick={() => setFlip(!flip)}>
-            <div className="front">
+        <div className={`flashcard ${flip ? "flip" : ""}`} onClick={() => setFlip(!flip)} style={{height: height}}>
+            <div className="front" ref={frontSide}>
                 {flashcard.question}
                 <div className="flashcard-options">
                     {flashcard.options.map(option => {
@@ -18,7 +35,7 @@ export default function Flashcard({ flashcard }) {
                 </div>
             </div>
 
-            <div className="back">{flashcard.answer}</div>
+            <div className="back" ref={backSide}>{flashcard.answer}</div>
         </div>
     )
 }
